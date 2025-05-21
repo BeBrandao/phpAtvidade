af6d9b96aafd650e18281a7dd8a3a2b3c579eebd
BeBrandao <beabs2305@gmail.com>
2025-05-21 10:19:13 -0300

    Update atvd2.php

--- atvd2.php
+++ atvd2.php
@@ -8,13 +8,27 @@
 <body>
 <h1>Atividade 1</h1>
 
-<form action="http://localhost/atvd1.php" method="post">
+<form action="http://localhost/atvd2.php" method="post">
     Número 1: <input type="number" name="num"><br><br>
     <button type="submit">verificar</button>
 </form>
 
 <?php
+    if (isset($_POST["num"])){
+        $num = $_POST["num"];
+        $resposta = "";
 
+        if ($num%10 === 0){
+            $resposta .= "<p>É divisivel por 10 </p><br>";
+        }
+        if ($num%5 === 0){
+            $resposta .= "<p>É divisivel por 5 </p><br>";
+        }
+        if ($num%2 === 0){
+            $resposta .= "<p>É divisivel por 2 </p><br>";
+        }
+    echo $resposta;
+    }
 ?>
 </body>
 </html>
